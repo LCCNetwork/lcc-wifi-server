e3e6f2880baa5a781dbdc3d3de453173bb4560f3
Aidan G <yayes.aidan@gmail.com>
2017-04-16 15:18:58 -0400

    work

--- src/app/app.js
+++ src/app/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const fs = require('fs')
 const bodyParser = require('body-parser')
+const cheerio = require('cheerio')
 const app = express()
 
 app.set('views', path.join(process.cwd(), 'build'))
