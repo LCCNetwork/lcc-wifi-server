a1892bc65e66b0f2ab3ff0dfc228647321a1a5fa
Aidan G <yayes.aidan@gmail.com>
2017-04-17 15:50:36 -0400

    data parse

--- src/app/app.js
+++ src/app/app.js
@@ -3,6 +3,8 @@ const path = require('path')
 const fs = require('fs')
 const bodyParser = require('body-parser')
 const cheerio = require('cheerio')
+const _eval = require('eval')
+const firebaseAdmin = require('firebase-admin')
 const app = express()
 
 app.set('views', path.join(process.cwd(), 'build'))
@@ -11,7 +13,49 @@ app.set('view engine', 'html')
 app.use(express.static(path.join(process.cwd(), 'build')))
 app.use(bodyParser.json())
 
-function loadUsers () { return JSON.parse(fs.readFileSync('./cfg/authUsers.json')) }
+const fbKey = require('../../cfg/lcc-wifi-firebase-adminsdk-7t3k9-909305e431.json')
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(fbKey),
+  databaseURL: 'https://lcc-wifi.firebaseio.com'
+})
+
+const authUsers = require('../../cfg/authUsers.json').users
+let users = []
+
+function getDataUsage (uid) {
+  return new Promise((resolve, reject) => {
+    fs.readFile('out.html', 'utf8', (err, file) => {
+      if (err) reject(err)
+      const $ = cheerio.load(file)
+      const user = users[uid]
+
+      const values = _eval($('script', 'body').text() + 'module.exports = values', '', {
+        document: {
+          write: function () {}
+        },
+        getSize: function () {}
+      })
+
+      let dataSum = 0
+
+      for (let device in values) {
+        device = values[device]
+        if (user.ips[device[2]]) dataSum += device[5]
+      }
+
+      resolve(dataSum)
+    })
+  })
+}
+
+class User {
+  constructor (email, uid, dataCap, ips) {
+    this.email = email
+    this.uid = uid
+    this.dataCap = dataCap
+    this.ips = ips
+  }
+}
 
 app.server = app.listen(8080, function () {
   const host = app.server.address().address
@@ -20,23 +64,49 @@ app.server = app.listen(8080, function () {
 })
 
 app.post('/auth', (req, res) => {
-  console.log(req.body)
+  console.log(req.body.token)
 
-  if (loadUsers().users.indexOf(req.body.email) !== -1) {
-    res.json({
-      auth: true
+  firebaseAdmin.auth().verifyIdToken(req.body.token).then((decodedToken) => {
+    console.log(decodedToken)
+    const uid = decodedToken.uid
+
+    firebaseAdmin.auth().getUser(uid).then((userRecord) => {
+      const email = userRecord.email
+
+      if (authUsers[email]) {
+        if (!users[uid]) users[uid] = new User(email, uid, authUsers[email], [req.ip])
+        else users[uid].ips[users[uid].ips.length] = req.ip
+
+        res.json({
+          auth: true
+        })
+      } else {
+        res.json({
+          auth: false
+        })
+      }
+    }).catch((err) => {
+      console.log('Token verification error: ' + err)
+      res.json({
+        auth: false
+      })
     })
-  } else {
+  }).catch((err) => {
+    console.log('Token verification error: ' + err)
     res.json({
       auth: false
     })
-  }
+  })
 })
 
 app.get('/user/:uid', (req, res) => {
-  res.json({
-    used: 1000,
-    total: 2000
+  getDataUsage(req.params.uid).then((used) => {
+    res.json({
+      used: used,
+      total: 2000
+    })
+  }).catch((err) => {
+    console.log(err)
   })
 })
 
