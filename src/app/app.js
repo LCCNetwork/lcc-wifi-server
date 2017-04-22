a54171ca48724d46295fd7f70dc612e61cb7a478
Aidan G <yayes.aidan@gmail.com>
2017-04-22 17:01:55 -0400

    fix iptables ips

--- src/app/app.js
+++ src/app/app.js
@@ -132,7 +132,7 @@ app.post('/auth', (req, res) => {
 
     firebaseAdmin.auth().getUser(uid).then((userRecord) => {
       const email = userRecord.email
-      const ip = req.ip.split(':')[-1]
+      const ip = req.ip.split(':').reverse()[1]
 
       if (authUsers[email]) {
         if (!users[uid]) users[uid] = new User(email, uid, authUsers[email], [ip])
