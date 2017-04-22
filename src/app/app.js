9081f33a9b8c83464d413a561e7ee2749d035332
CF12 <brianx0011@gmail.com>
2017-04-22 14:45:39 -0400

    Implemented CORS support

--- src/app/app.js
+++ src/app/app.js
@@ -126,9 +126,10 @@ app.server = app.listen(8080, function () {
 })
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next()
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  if (req.method === 'OPTIONS') { res.sendStatus(200) }
+  else { next() }
 })
 
 app.post('/auth', (req, res) => {
